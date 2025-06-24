dba59f7b3dcf324682be956b7cc2cb2b074347c8
小威生吃小孩 <15853373+xiao-wei-eats-raw-children@user.noreply.gitee.com>
2025-06-24 09:25:22 +0800

    5 - 7题

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -35,34 +35,93 @@ class Solution {
 
     public int lengthOfLongestSubstring(String s) {
         int result = 0;
-        HashMap<Character , Integer> hashMap = new HashMap<>();
+        HashMap<Character, Integer> hashMap = new HashMap<>();
         char[] charArray = s.toCharArray();
         int i = 0;
         int j = 0;
-        while (j < charArray.length){
-            if(hashMap.containsKey(charArray[j]) && hashMap.get(charArray[j]) >= i){
-                result = Integer.max(result ,j - i);
+        while (j < charArray.length) {
+            if (hashMap.containsKey(charArray[j]) && hashMap.get(charArray[j]) >= i) {
+                result = Integer.max(result, j - i);
                 i = hashMap.get(charArray[j]) + 1;
             }
-            hashMap.put(charArray[j] , j++);
+            hashMap.put(charArray[j], j++);
         }
-        return Integer.max(result , j - i);
+        return Integer.max(result, j - i);
     }
 
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int i =0;
+        int i = 0;
         int j = 0;
         int now = -1;
         int last = -1;
         int length = nums1.length + nums2.length;
-        for (int k = 0; k <= length/2; k++) {
+        for (int k = 0; k <= length / 2; k++) {
             last = now;
-            if(i < nums1.length && (j >= nums2.length || nums1[i] < nums2[j])) now = nums1[i++];
-            else now =nums2[j++];
+            if (i < nums1.length && (j >= nums2.length || nums1[i] < nums2[j])) now = nums1[i++];
+            else now = nums2[j++];
         }
-        if(length % 2==0) return (last + now)/2.0;
+        if (length % 2 == 0) return (last + now) / 2.0;
         else return now;
+    }
+
+
+    public String longestPalindrome(String s) {
+        char[] charArray = s.toCharArray();
+        boolean[][] dp = new boolean[charArray.length + 2][charArray.length + 2];
+        int length = 0;
+        int start = 0;
+        int end = 0;
+        for (int i = charArray.length; i >= 1; i--) {
+            for (int j = i; j <= charArray.length; j++) {
+                if (charArray[i - 1] == charArray[j - 1] && ((dp[i + 1][j - 1]) || i + 2 > j)) {
+                    dp[i][j] = true;
+                    if (j - i + 1 > length) {
+                        length = j - i + 1;
+                        start = i - 1;
+                        end = j - 1;
+                    }
+                }
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public String convert(String s, int numRows) {
+        StringBuilder result = new StringBuilder();
+        char[] charArray = s.toCharArray();
+
+        int length = (numRows - 1) * 2;
+        if (length == 0) length = 1;
+
+        for (int i = 0; i < numRows; i++) {
+            int j = 0;
+            while (i + j * length < charArray.length) {
+                if (i == 0 || i == numRows - 1) result.append(charArray[i + j * length]);
+                else {
+                    result.append(charArray[i + j * length]);
+                    if ((j + 1) * length - i < charArray.length) result.append(charArray[(j + 1) * length - i]);
+                }
+                j++;
+            }
+        }
+
+        return result.toString();
+    }
+
+    public int reverse(int x) {
+        long num = x;
+        num = Math.abs(num);
+        long result = 0;
+
+        while (num != 0) {
+            result = result * 10 + num % 10;
+            num /= 10;
+        }
+        if (x < 0) result = -result;
+        if(result > Integer.MAX_VALUE || result < Integer.MIN_VALUE) return 0;
+        return (int)result;
     }
 
 
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -18,16 +18,38 @@ public class SolutionTest {
     }
 
     @Test
-    public void lengthOfLongestSubstringTest(){
-        Assert.assertEquals(3 , solution.lengthOfLongestSubstring("abcabcbb"));
-        Assert.assertEquals(1 , solution.lengthOfLongestSubstring("bbbbb"));
-        Assert.assertEquals(3 , solution.lengthOfLongestSubstring("pwwkew"));
+    public void lengthOfLongestSubstringTest() {
+        Assert.assertEquals(3, solution.lengthOfLongestSubstring("abcabcbb"));
+        Assert.assertEquals(1, solution.lengthOfLongestSubstring("bbbbb"));
+        Assert.assertEquals(3, solution.lengthOfLongestSubstring("pwwkew"));
     }
 
     @Test
-    public void findMedianSortedArraysTest(){
-        Assert.assertEquals(2 ,solution.findMedianSortedArrays(new int[]{1,3} , new int[]{2}) , 0);
-        Assert.assertEquals(2.5 ,solution.findMedianSortedArrays(new int[]{1,2} ,new int[]{3 , 4}) , 0);
+    public void findMedianSortedArraysTest() {
+        Assert.assertEquals(2, solution.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}), 0);
+        Assert.assertEquals(2.5, solution.findMedianSortedArrays(new int[]{1, 2}, new int[]{3, 4}), 0);
+    }
+
+    @Test
+    public void longestPalindromeTest() {
+        Assert.assertTrue(solution.longestPalindrome("babad").equals("bab") || solution.longestPalindrome("babad").equals("aba"));
+        Assert.assertEquals("bb", solution.longestPalindrome("cbbd"));
+        Assert.assertEquals("aca", solution.longestPalindrome("aacabdkacaa"));
+    }
+
+    @Test
+    public void convertTest() {
+        Assert.assertEquals("PAHNAPLSIIGYIR", solution.convert("PAYPALISHIRING", 3));
+        Assert.assertEquals("PINALSIGYAHRPI", solution.convert("PAYPALISHIRING", 4));
+        Assert.assertEquals("A", solution.convert("A", 1));
+    }
+
+    @Test
+    public void reverseTest(){
+        Assert.assertEquals(321 ,solution.reverse(123));
+        Assert.assertEquals(-321 ,solution.reverse(-123));
+        Assert.assertEquals(21 ,solution.reverse(120));
+        Assert.assertEquals(0 ,solution.reverse(0));
     }
 
 }
